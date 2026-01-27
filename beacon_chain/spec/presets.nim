23a7a48ba56b422f684d0b3d44fed1bcf4cf0f6d
Samyxandz <tamaghna.official@gmail.com>
2026-01-27 19:55:56 +0530

    minimal preset fix

--- beacon_chain/spec/presets.nim
+++ beacon_chain/spec/presets.nim
@@ -400,6 +400,199 @@ when const_preset == "mainnet":
     MIN_EPOCHS_FOR_DATA_COLUMN_SIDECARS_REQUESTS: 4096,
   )
 
+elif const_preset == "minimal":
+  import ./presets/minimal
+  export minimal
+
+  # https://github.com/ethereum/consensus-specs/blob/v1.4.0-beta.5/configs/minimal.yaml
+  const defaultRuntimeConfig* = RuntimeConfig(
+    # Minimal config
+
+    # Extends the minimal preset
+    PRESET_BASE: "minimal",
+
+    # Free-form short name of the network that this configuration applies to - known
+    # canonical network names include:
+    # * 'minimal' - spec-testing
+    # Must match the regex: [a-z0-9\-]
+    CONFIG_NAME: "minimal",
+
+    # Transition
+    # ---------------------------------------------------------------
+    # 2**256-2**10 for testing minimal network
+    TERMINAL_TOTAL_DIFFICULTY:
+      u256"115792089237316195423570985008687907853269984665640564039457584007913129638912",
+    # By default, don't use these params
+    TERMINAL_BLOCK_HASH:
+      hash32"0x0000000000000000000000000000000000000000000000000000000000000000",
+
+
+    # Genesis
+    # ---------------------------------------------------------------
+    # [customized]
+    MIN_GENESIS_ACTIVE_VALIDATOR_COUNT: 64,
+    # Jan 3, 2020
+    MIN_GENESIS_TIME: 1578009600,
+    # Highest byte set to 0x01 to avoid collisions with mainnet versioning
+    GENESIS_FORK_VERSION: Version [byte 0x00, 0x00, 0x00, 0x01],
+    # [customized] Faster to spin up testnets, but does not give validator reasonable warning time for genesis
+    GENESIS_DELAY: 300,
+
+
+    # Forking
+    # ---------------------------------------------------------------
+    # Values provided for illustrative purposes.
+    # Individual tests/testnets may set different values.
+
+    # Altair
+    ALTAIR_FORK_VERSION: Version [byte 0x01, 0x00, 0x00, 0x01],
+    ALTAIR_FORK_EPOCH: Epoch(uint64.high),
+    # Bellatrix
+    BELLATRIX_FORK_VERSION: Version [byte 0x02, 0x00, 0x00, 0x01],
+    BELLATRIX_FORK_EPOCH: Epoch(uint64.high),
+    # Capella
+    CAPELLA_FORK_VERSION: Version [byte 0x03, 0x00, 0x00, 0x01],
+    CAPELLA_FORK_EPOCH: Epoch(uint64.high),
+    # Deneb
+    DENEB_FORK_VERSION: Version [byte 0x04, 0x00, 0x00, 0x01],
+    DENEB_FORK_EPOCH: Epoch(uint64.high),
+    # Electra
+    ELECTRA_FORK_VERSION: Version [byte 0x05, 0x00, 0x00, 0x01],
+    ELECTRA_FORK_EPOCH: Epoch(uint64.high),
+    # Fulu
+    FULU_FORK_VERSION: Version [byte 0x06, 0x00, 0x00, 0x01],
+    FULU_FORK_EPOCH: Epoch(uint64.high),
+    # Gloas
+    GLOAS_FORK_VERSION: Version [byte 0x07, 0x00, 0x00, 0x01],
+    GLOAS_FORK_EPOCH: Epoch(uint64.high),
+
+    # Time parameters
+    # ---------------------------------------------------------------
+    timeParams: TimeParams(
+      # [customized] 6000 milliseconds
+      SLOT_DURATION: milliseconds(6000),
+
+      # 1667 basis points, ~17% of SLOT_DURATION_MS
+      PROPOSER_REORG_CUTOFF_BPS: 1667,
+      # 3333 basis points, ~33% of SLOT_DURATION_MS
+      ATTESTATION_DUE_BPS: 3333,
+      # 6667 basis points, ~67% of SLOT_DURATION_MS
+      AGGREGATE_DUE_BPS: 6667,
+
+      # Altair
+      # 3333 basis points, ~33% of SLOT_DURATION_MS
+      SYNC_MESSAGE_DUE_BPS: 3333,
+      # 6667 basis points, ~67% of SLOT_DURATION_MS
+      CONTRIBUTION_DUE_BPS: 6667,
+
+      # Gloas
+      # 2500 basis points, ~25% of SLOT_DURATION_MS
+      ATTESTATION_DUE_BPS_GLOAS: 2500,
+      # 5000 basis points, ~50% of SLOT_DURATION_MS
+      AGGREGATE_DUE_BPS_GLOAS: 5000,
+      # 2500 basis points, ~25% of SLOT_DURATION_MS
+      SYNC_MESSAGE_DUE_BPS_GLOAS: 2500,
+      # 5000 basis points, ~50% of SLOT_DURATION_MS
+      CONTRIBUTION_DUE_BPS_GLOAS: 5000,
+      # 7500 basis points, ~75% of SLOT_DURATION_MS
+      PAYLOAD_ATTESTATION_DUE_BPS: 7500),
+
+    # 14 (estimate from Eth1 mainnet)
+    SECONDS_PER_ETH1_BLOCK: 14,
+    # 2**8 (= 256) epochs
+    MIN_VALIDATOR_WITHDRAWABILITY_DELAY: 256,
+    # [customized] higher frequency of committee turnover and faster time to acceptable voluntary exit
+    SHARD_COMMITTEE_PERIOD: 64,
+    # [customized] process deposits more quickly, but insecure
+    ETH1_FOLLOW_DISTANCE: 16,
+
+    # Validator cycle
+    # ---------------------------------------------------------------
+    # 2**2 (= 4)
+    INACTIVITY_SCORE_BIAS: 4,
+    # 2**4 (= 16)
+    INACTIVITY_SCORE_RECOVERY_RATE: 16,
+    # 2**4 * 10**9 (= 16,000,000,000) Gwei
+    EJECTION_BALANCE: 16000000000'u64,
+    # [customized] more easily demonstrate the difference between this value and the activation churn limit
+    MIN_PER_EPOCH_CHURN_LIMIT: 2,
+    # [customized] scale queue churn at much lower validator counts for testing
+    CHURN_LIMIT_QUOTIENT: 32,
+    # [New in Deneb:EIP7514] [customized]
+    MAX_PER_EPOCH_ACTIVATION_CHURN_LIMIT: 4,
+
+    # Fork choice
+    # ---------------------------------------------------------------
+    REORG_HEAD_WEIGHT_THRESHOLD: 20,
+    REORG_MAX_EPOCHS_SINCE_FINALIZATION: 2,
+
+    # Deposit contract
+    # ---------------------------------------------------------------
+    # Ethereum Goerli testnet
+    DEPOSIT_CHAIN_ID: 5,
+    DEPOSIT_NETWORK_ID: 5,
+    # Configured on a per testnet basis
+    DEPOSIT_CONTRACT_ADDRESS: default(Eth1Address),
+
+    # Networking
+    # ---------------------------------------------------------------
+    # `10 * 2**20` (= 10485760, 10 MiB)
+    # TODO MAX_PAYLOAD_SIZE: 10485760,
+    # `2**10` (= 1024)
+    # TODO MAX_REQUEST_BLOCKS: 1024,
+    # `2**8` (= 256)
+    # TODO EPOCHS_PER_SUBNET_SUBSCRIPTION: 256,
+    # [customized] `MIN_VALIDATOR_WITHDRAWABILITY_DELAY + CHURN_LIMIT_QUOTIENT // 2` (= 272)
+    MIN_EPOCHS_FOR_BLOCK_REQUESTS: 272,
+    # TODO ATTESTATION_PROPAGATION_SLOT_RANGE: 32,
+    # 500ms
+    # TODO MAXIMUM_GOSSIP_CLOCK_DISPARITY: 500,
+    # TODO MESSAGE_DOMAIN_INVALID_SNAPPY: [byte 0x00, 0x00, 0x00, 0x00],
+    # TODO MESSAGE_DOMAIN_VALID_SNAPPY: [byte 0x01, 0x00, 0x00, 0x00],
+    # 2 subnets per node
+    # TODO SUBNETS_PER_NODE: 2,
+    # 2**8 (= 64)
+    # TODO ATTESTATION_SUBNET_COUNT: 64,
+    # TODO ATTESTATION_SUBNET_EXTRA_BITS: 0,
+    # ceillog2(ATTESTATION_SUBNET_COUNT) + ATTESTATION_SUBNET_EXTRA_BITS
+    # TODO ATTESTATION_SUBNET_PREFIX_BITS: 6,
+
+    # Deneb
+    # `2**7` (=128)
+    # TODO MAX_REQUEST_BLOCKS_DENEB: 128,
+    # `2**12` (= 4096 epochs, ~18 days)
+    MIN_EPOCHS_FOR_BLOB_SIDECARS_REQUESTS: 4096,
+    # `6`
+    BLOB_SIDECAR_SUBNET_COUNT: 6,
+    # `uint64(6)`
+    MAX_BLOBS_PER_BLOCK: 6,
+    # MAX_REQUEST_BLOCKS_DENEB * MAX_BLOBS_PER_BLOCK
+    MAX_REQUEST_BLOB_SIDECARS: 768,
+
+    # Electra
+    # [customized] 2**6 * 10**9 (= 64,000,000,000)
+    MIN_PER_EPOCH_CHURN_LIMIT_ELECTRA: 64000000000'u64,
+    # [customized] 2**7 * 10**9 (= 128,000,000,000)
+    MAX_PER_EPOCH_ACTIVATION_EXIT_CHURN_LIMIT: 128000000000'u64,
+    # `9`
+    BLOB_SIDECAR_SUBNET_COUNT_ELECTRA: 9,
+    # `uint64(9)`
+    MAX_BLOBS_PER_BLOCK_ELECTRA: 9,
+    # MAX_REQUEST_BLOCKS_DENEB * MAX_BLOBS_PER_BLOCK_ELECTRA
+    MAX_REQUEST_BLOB_SIDECARS_ELECTRA: 1152,
+
+    # Fulu
+    NUMBER_OF_COLUMNS: 128,
+    NUMBER_OF_CUSTODY_GROUPS: 128,
+    DATA_COLUMN_SIDECAR_SUBNET_COUNT: 128,
+    MAX_REQUEST_DATA_COLUMN_SIDECARS: 16384,
+    SAMPLES_PER_SLOT: 8,
+    CUSTODY_REQUIREMENT: 4,
+    VALIDATOR_CUSTODY_REQUIREMENT: 8,
+    BALANCE_PER_ADDITIONAL_CUSTODY_GROUP: 32000000000'u64,
+    MIN_EPOCHS_FOR_DATA_COLUMN_SIDECARS_REQUESTS: 4096,
+  )
+
 
 else:
   {.error: "Only mainnet and minimal presets supported".}
--- docker/dist/README.md.tpl
+++ docker/dist/README.md.tpl
@@ -63,8 +63,12 @@ WEB3_URL="ws://localhost:8545" ./run-mainnet-beacon-node.sh
 
 ## Testnet
 
+The `hoodi` testnet runs on
 
 ```bash
+# using a local hoodi instance
 build/nimbus_beacon_node \
+    --network=hoodi \
+    --data-dir=build/data/shared_hoodi_0 \
     --web3-url="ws://localhost:8545"
 ```
